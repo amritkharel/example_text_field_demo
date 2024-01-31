ac4249e1ce5d85f327921b3f70594103efbd6feb
Amritkharel <amrit.kharel16@gmail.com>
2024-01-31 14:34:17 +0545

    basic list and gif selection implementation is ongoing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:extended_text_field/extended_text_field.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,86 +14,172 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<String> messages = ["Hello", "World"];
+  final TextEditingController _controller = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Extended Text Field Demo"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              reverse: true,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return CustomMessage(message: messages[index]);
+              },
+            ),
+          ),
+          Expanded(child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: ExtendedTextField(
+              controller: _controller,
+              specialTextSpanBuilder: MySpecialTextSpanBuilder(), // Implement this to handle GIF or special inputs
+              decoration: InputDecoration(
+                hintText: "Type a message",
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    final inputText = _controller.text;
+                    // Check if the inputText contains a GIF reference
+                    if (inputText.contains(GifText.flag)) {
+                      // Handle GIF
+                      final gifId = inputText.substring(inputText.indexOf(GifText.flag) + GifText.flag.length, inputText.indexOf(']'));
+                      print("Sending GIF with ID: $gifId");
+                      // Here you would send the GIF based on its ID
+                    } else {
+                      // Handle regular text
+                      print("Sending message: $inputText");
+                      // Here you would send the message as text
+                    }
+                  },
+                ),
+              ),
+            ),
+          ),),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMessage extends StatelessWidget {
+  final String message;
+
+  const CustomMessage({required this.message, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double maxScreenWidth = MediaQuery.of(context).size.width;
+    double maxScreenHeight = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: maxScreenWidth * 0.9,
+      height: maxScreenHeight * 0.1,
+      color: Colors.grey,
+      child: ClipRRect(
+        clipBehavior: Clip.hardEdge,
+        borderRadius: BorderRadius.circular(10.0),
+        child: Text(
+          message,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
+
+class ExtendedTextFieldImplementation extends StatefulWidget {
+  const ExtendedTextFieldImplementation({super.key});
+
+  @override
+  State<ExtendedTextFieldImplementation> createState() => _ExtendedTextFieldImplementationState();
+}
+
+class _ExtendedTextFieldImplementationState extends State<ExtendedTextFieldImplementation> {
+
+  @override
+  void initState() {
+    super.initState();
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return const Placeholder();
+  }
+}
+
+
+
+class GifText extends SpecialText {
+  final TextStyle? textStyle;
+  // Keep the base class signature for onTap
+  final void Function(dynamic)? onTap;
+
+  GifText(this.textStyle, {this.onTap, required int start}) : super(GifText.flag, ']', textStyle);
+
+  static const String flag = '[GIF:';
+
+  @override
+  InlineSpan finishText() {
+    final String key = getContent();
+
+    GestureRecognizer? recognizer = onTap != null ? (TapGestureRecognizer()..onTap = () => onTap!(null)) : null;
+
+    return SpecialTextSpan(
+      text: '', // Placeholder text, you could customize it
+      actualText: key,
+      style: textStyle?.copyWith(color: Colors.transparent),
+      recognizer: recognizer,
+      children: <InlineSpan>[
+        WidgetSpan(
+          alignment: PlaceholderAlignment.middle,
+          child: Container(
+            // Simulated GIF representation
+            padding: EdgeInsets.all(2),
+            child: Text('[GIF]', style: TextStyle(color: Colors.blue, fontSize: 16)),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
+
+class MySpecialTextSpanBuilder extends SpecialTextSpanBuilder {
+  @override
+  SpecialText? createSpecialText(String flag, {TextStyle? textStyle, void Function(dynamic)? onTap, required int index}) {
+    if (flag.startsWith(GifText.flag)) {
+      return GifText(textStyle, onTap: onTap, start: index);
+    }
+    return null;
+  }
+}
+
+
